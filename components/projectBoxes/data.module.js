419c0941467f3795a56b5693745ece014772323d
ChanHK <59449120+ChanHK@users.noreply.github.com>
2023-06-18 22:18:05 +0800

    Update data.module.js

--- components/projectBoxes/data.module.js
+++ components/projectBoxes/data.module.js
@@ -4,28 +4,35 @@ const data = [
     url: "https://sort-visualizer-1y9ikkjm3-chanhk.vercel.app/",
     github: "https://github.com/ChanHK/SortVisualizer",
     des: "Provides a visual representation of how different sorting algorithms work to sort data, allowing users to gain a better understanding of the sorting process.",
-    tech: "NextJS JavaScript Bootstrap"
+    tech: "NextJS JavaScript Bootstrap",
+  },
+  {
+    name: "Shortify",
+    url: "https://shoritfy-atz97ta24-chanhk.vercel.app/",
+    github: "https://github.com/ChanHK/Shortify",
+    des: "This URL Shortener Microservice is a web application that allows users to shorten long URLs into shorter and more manageable links. It provides an API interface for creating and managing shortened URLs.",
+    tech: "Nodejs Express MongoDB Jest RestApi",
   },
   {
     name: "The Pair Game",
     url: "https://the-pair-game-aj00nv8jp-chanhk.vercel.app/",
     github: null,
     des: "A mini game that challenges users to tap pairs of tiles to reveal if they match.",
-    tech: "NextJS CSS Javascript"
+    tech: "NextJS CSS Javascript",
   },
   {
     name: "Parallex Star Background",
     url: "https://chanhk.github.io/Parallax-Star-Background/",
     github: "https://github.com/ChanHK/Parallax-Star-Background",
     des: "A simple, built for fun parallax star background.",
-    tech: "HTML CSS Less"
+    tech: "HTML CSS Less",
   },
   {
     name: "Animation Gallery",
     url: "https://chanhk.github.io/AnimationGallery/",
     github: "https://github.com/ChanHK/AnimationVault",
     des: "Showcase animations that I built for fun.",
-    tech: "HTML CSS"
+    tech: "HTML CSS",
   },
   // {
   //   name: "",
